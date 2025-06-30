5364c6f0c919a378daf6a1da00728f6cf83436ae
Stijn van Lierop (DBS) <stijn@hkqd5x3-l.holmes.nl>
2025-06-30 15:21:45 +0200

    first version of FF++ dataset class

--- deepfake_detection/utils/parameters.py
+++ deepfake_detection/utils/parameters.py
@@ -1,3 +1,4 @@
+from deepfake_detection.data.datasets.FaceForensicsDataset import FaceForensicsDataset
 from deepfake_detection.data.datasets.FileImageDataset import FileImageDataset
 from deepfake_detection.data.datasets.FileVideoDataset import FileVideoDataset
 from deepfake_detection.data.datasets.GenImageDataset import GenImageDataset
@@ -9,7 +10,8 @@ from deepfake_detection.models.detection.naive.resnet50 import ResNet50
 # Define the possible dataset classes that can be initialized
 DATASETS = {'FileImageDataset' : FileImageDataset,
             'FileVideoDataset' : FileVideoDataset,
-            'GenImageDataset' : GenImageDataset}
+            'GenImageDataset' : GenImageDataset,
+            'FaceForensicsDataset': FaceForensicsDataset}
 
 # Define the possible model classes that can be initialized
 MODELS = {'Cozzolino2023' : Cozzolino2023Model,
