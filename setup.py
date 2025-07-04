6a3e21d5f8792e575341349bbecd61224b4fe88c
Stijn van Lierop (DBS) <stijn@hkqd5x3-l.holmes.nl>
2025-07-04 13:24:22 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+import re
+from setuptools import setup, find_packages
+
+
+if __name__ == '__main__':
+    with open("README.md", "r") as f:
+        readme = f.read()
+
+    with open("requirements.txt", "r") as f:
+        dependencies = list(
+            filter(
+                lambda x: not re.search(r"^(--|#)", x),
+                map(str.strip, f.read().split("\n"))
+            )
+        )
+    setup(
+        name="deepfake_detection",
+        version="0.0.0",
+        description="A library for deepfake detection research.",
+        long_description=readme,
+        author="Stijn van Lierop",
+        author_email="s.van.lierop@nfi.nl",
+        url="https://github.com/StijnvLierop/DeepfakeDetection",
+        packages=find_packages(exclude=["tests", "tests.*"]),
+        install_requires=dependencies,
+        extras_require={
+            "dev": [
+                "pytest",
+                "flake8",
+            ],
+        },
+        python_requires=">=3.10",
+        classifiers=[
+            "Programming Language :: Python :: 3",
+        ],
+    )
