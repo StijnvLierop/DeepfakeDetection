4010f6d6921509420ebd99700fe1f827644f4c8c
Stijn van Lierop (DBS) <stijn@hkqd5x3-l.holmes.nl>
2025-07-08 13:19:16 +0200

    implemented stratified and shuffled splitting

--- tests/deepfake_detection/data/test_split.py
+++ tests/deepfake_detection/data/test_split.py
@@ -1,23 +1,18 @@
-import numpy as np
+import collections
+
 import pytest
 
 from deepfake_detection.data import Instance, Dataset
-from deepfake_detection.data.datasets import ListDataset
 from deepfake_detection.data import split_dataset
+from deepfake_detection.data.datasets import ListDataset
 
 
 @pytest.fixture
 def dataset() -> Dataset:
-    return ListDataset(instances=[Instance(str(idx), labels) for idx, labels in zip(range(8), (
-        {"A"},
-        {"A"},
-        {"A", "B", "C"},
-        {"A", "C"},
-        {"B", "C"},
-        {"B", "C"},
-        {"B", "C"},
-        {"C"}
-    ))])
+    return ListDataset(
+        instances=[Instance(str(idx), labels) for idx, labels in
+                            zip(range(15), ["A", "A", "B", "C", "C", "C", "B", "C", "C", "A", "A", "B", "C", "A", "B"])]
+                       )
 
 def test_split_dataset_size(dataset: Dataset):
     train_set, test_set = split_dataset(dataset, test_size=0.2, random_state=42)
@@ -46,3 +41,16 @@ def test_split_dataset_leakage(dataset: Dataset):
 def test_split_dataset_data_unaltered(dataset: Dataset):
     train_set, test_set = split_dataset(dataset, test_size=0.2, random_state=42)
     assert set(train_set.instances + test_set.instances) == set(dataset)
+
+
+def test_split_dataset_stratified(dataset: Dataset):
+    train_set, test_set = split_dataset(dataset, test_size=0.2, random_state=42, stratify=True)
+    counter_train = collections.Counter([instance.label for instance in train_set])
+    counter_test = collections.Counter([instance.label for instance in test_set])
+    counter_original = collections.Counter([instance.label for instance in dataset])
+    for label in counter_original:
+        train_ratio = counter_train[label] / len(train_set)
+        test_ratio = counter_test[label] / len(test_set)
+        original_ratio = counter_original[label] / len(dataset)
+        assert abs(train_ratio - original_ratio) < 0.1
+        assert abs(test_ratio - original_ratio) < 0.1
