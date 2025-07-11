073dce9cf9c1c0269055d841d9de32717823ad4c
Stijn van Lierop (DBS) <stijn@hkqd5x3-l.holmes.nl>
2025-07-11 21:13:36 +0200

    feat: add split_file parameter to FileVideoDataset to enable retrieval of instances specified in a given file

--- deepfake_detection/data/datasets/filevideodataset.py
+++ deepfake_detection/data/datasets/filevideodataset.py
@@ -4,7 +4,6 @@ from typing import Iterable
 from deepfake_detection.data.dataset import Dataset
 from deepfake_detection.data.instance import VideoInstance
 
-
 class FileVideoDataset(Dataset):
     """
     This dataset loads a dataset of videos from a filesystem.
@@ -23,11 +22,19 @@ class FileVideoDataset(Dataset):
 
     :param path: The path to the root folder of the dataset.
     :param name: The name of the dataset.
+    :param split_file: The path to a file containing the filenames of the videos that should be returned.
     """
 
-    def __init__(self, path: str, name: str = None):
+    def __init__(self, path: str, name: str = None, split_file: str = None):
         super().__init__(name)
         self.path = path
+
+        # If split file provided store the filenames of included instances in a list
+        if split_file:
+            with open(split_file, 'r') as f:
+                self.included_instances = f.read().splitlines()
+        else:
+            self.included_instances = None
 
     def __len__(self):
         """
@@ -45,7 +52,8 @@ class FileVideoDataset(Dataset):
                         # Loop over videos
                         for video in os.listdir(os.path.join(self.path, folder, subfolder)):
                                 if video.split('.')[-1].lower() in ['mp4', 'mov']:
-                                    n += 1
+                                    if self.included_instances is None or video in self.included_instances:
+                                        n += 1
         return n
 
     @property
@@ -76,7 +84,8 @@ class FileVideoDataset(Dataset):
                         # Loop over videos
                         for video in os.listdir(os.path.join(self.path, folder, subfolder)):
                             if video.split('.')[-1].lower() in ['mp4', 'mov']:
-                                yield VideoInstance(os.path.join(self.path, folder, subfolder, video),
-                                                    subfolder)
+                                if self.included_instances is None or video in self.included_instances:
+                                    yield VideoInstance(os.path.join(self.path, folder, subfolder, video),
+                                                        subfolder)
                             else:
                                 raise print("Found file that is not a mp4 or mov file: {}".format(video))
