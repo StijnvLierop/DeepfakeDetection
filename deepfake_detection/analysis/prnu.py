0b56000bac0cdf5f607ec5e4941b75ccb8e134b3
Stijn van Lierop (DBS) <stijn@hkqd5x3-l.holmes.nl>
2025-07-31 14:46:48 +0200

    feat: add new functionality to compute PRNU pattern using 2nd order FSTV method

---
+++ deepfake_detection/analysis/prnu.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+from deepfake_detection.data import ImageInstance
+
+
+def prnu_fstv(instance: ImageInstance) -> np.ndarray:
+    """
+    This function extracts the PRNU pattern from a given image instance using the
+    2nd order First Step Total Variation (FSTV) method (https://doi.org/10.1016/j.diin.2013.08.002).
+
+    :param instance: Instance of FileImageInstance to extract PRNU from.
+    :return: ImageInstance containing the extracted PRNU.
+    """
+    # Get image data (ensure float32 for numerical precision)
+    u0 = np.array(instance.data).astype(np.float32)
+
+    # Compute gradients
+    grad_x = np.gradient(u0, axis=1)
+    grad_y = np.gradient(u0, axis=0)
+
+    # Calculate the strength of the gradients (add small epsilon to avoid taking the square root of 0)
+    epsilon = 1e-8
+    grad_mag = np.sqrt(grad_x ** 2 + grad_y ** 2 + epsilon ** 2)
+
+    # Normalize gradients
+    norm_grad_x = grad_x / grad_mag
+    norm_grad_y = grad_y / grad_mag
+
+    # Compute 2nd order gradients (gradient of normalized gradients)
+    div_norm_x = np.gradient(norm_grad_x, axis=1)
+    div_norm_y = np.gradient(norm_grad_y, axis=0)
+    divergence = div_norm_x + div_norm_y
+
+    # The noise is then defined as the negative divergence
+    noise = -divergence
+
+    return noise
