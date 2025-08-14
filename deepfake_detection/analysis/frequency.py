b0794178f54a188f0ae27b2df4368339ae1deeaa
Stijn van Lierop (DBS) <stijn@hkqd5x3-l.holmes.nl>
2025-08-14 15:26:51 +0200

    feat(fft): add parameter to allow for optional filtering using hamming window

--- deepfake_detection/analysis/frequency.py
+++ deepfake_detection/analysis/frequency.py
@@ -1,15 +1,30 @@
 import numpy as np
+from skimage.filters import window
 
 
-def fft(img: np.ndarray) -> np.ndarray:
+def fft(img: np.ndarray, hamming_window: bool=False) -> np.ndarray:
     """
     Performs a Fast Fourier Transform (FFT) on an image and extracts the magnitude spectrum for visualization.
 
     :param img: A numpy array containing the image data to be transformed of the form
                 (height, width) or (height, width, channels). Note that taking the fft of all color channels
                 individually is different from taking the fft of a 3d image once.
+    :param hamming_window: Whether to apply a Hamming window to the image before performing the FFT. This removes
+                           the prominent horizontal and vertical frequency bands caused by the image edges due to
+                           violating the periodicity assumption of the FFT.
     :return: A numpy array containing the transformed image (magnitude spectrum) of the form (height, width, channels).
     """
+    # Apply Hamming window if set
+    if hamming_window:
+        # If color image
+        if img.ndim == 3:
+            # Apply window for every channel
+            for channel in range(img.shape[2]):
+                img[:, :, channel] = img[:, :, channel] * window('hamming', img.shape[:2])
+        # If grayscale image, apply once
+        else:
+            img = img * window('hamming', img.shape)
+
     # If array is 3D, transform so channel dimension comes first
     if img.ndim == 3:
         img = np.transpose(img, (2, 0, 1))
