0f9af94ebc6a9f34c7ce0831b5e6c86cbb5bda19
Stijn van Lierop <150940112+StijnvLierop@users.noreply.github.com>
2025-12-22 09:36:03 +0100

    feat: add support for huggingface trainer and trainingarguments

--- deepfake_detection/data/datasets/torch.py
+++ deepfake_detection/data/datasets/torch.py
@@ -1,24 +1,8 @@
-from typing import Tuple
-
 import numpy as np
 import torch
+from torchvision.transforms import v2
 
-from deepfake_detection.data import Instance, ImageInstance, FileImageInstance
 from deepfake_detection.data.dataset import Dataset, MapStyleDatasetMixin
-
-
-def instance_to_torch_dict(instance: Instance) -> Tuple[torch.Tensor, torch.Tensor]:
-    """
-    This function transforms an Instance to a dict that can be loaded by a Pytorch DataLoader.
-    """
-    # Transform instance data to tensor
-    if isinstance(instance, ImageInstance) or isinstance(instance, FileImageInstance):
-        input_tensor = torch.tensor(np.array(instance.data), dtype=torch.uint8).permute(2, 0, 1)
-    # TODO: add audio and video instance
-    else:
-        raise ValueError(f"Instance type {type(instance)} cannot be converted to torch tensor.")
-
-    return input_tensor, torch.tensor(instance.annotation.binary_label, dtype=torch.int8)
 
 
 class TorchDataset(torch.utils.data.Dataset):
@@ -26,12 +10,16 @@ class TorchDataset(torch.utils.data.Dataset):
     A wrapper to load a dataset as a Pytorch Map-style dataset.
     """
 
-    def __init__(self, dataset: MapStyleDatasetMixin):
+    def __init__(self, dataset: MapStyleDatasetMixin, transform: v2.Compose):
         super().__init__()
         self.dataset = dataset
+        self.transforms = transform
 
     def __getitem__(self, idx: int):
-        return instance_to_torch_dict(self.dataset[idx])
+        instance = self.dataset[idx]
+        return {'inputs': self.transforms(instance.data),
+                'label': torch.tensor(instance.annotation.binary_label, dtype=torch.uint8)
+                }
 
     def __len__(self):
         return len(self.dataset)
@@ -42,13 +30,16 @@ class TorchIterableDataset(torch.utils.data.IterableDataset):
     A wrapper to load a dataset as a Pytorch iterable dataset.
     """
 
-    def __init__(self, dataset: Dataset):
+    def __init__(self, dataset: Dataset, transform: v2.Compose):
         super().__init__()
         self.dataset = dataset
+        self.transforms = transform
 
     def __len__(self):
         return len(self.dataset)
 
     def __iter__(self):
         for instance in self.dataset:
-            yield instance_to_torch_dict(instance)
+            yield {'inputs': self.transforms(instance.data),
+                   'label': torch.tensor(instance.annotation.binary_label, dtype=torch.int8)
+                   }
