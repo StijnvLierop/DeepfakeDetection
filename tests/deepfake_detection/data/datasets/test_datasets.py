5cb8205dfcc31e9c815d16d33d4382e7c05a2f46
stijn <s.van.lierop@nfi.nl>
2026-01-09 14:20:27 +0100

    fix: unit tests

--- tests/deepfake_detection/data/datasets/test_datasets.py
+++ tests/deepfake_detection/data/datasets/test_datasets.py
@@ -97,7 +97,7 @@ def test_dataset_different_instances_different(image_dataset_path):
 
 def test_filtered_dataset_iteration(dummy_dataset):
     def indices():
-        return lambda x: [2, 4]
+        return [2, 4]
     filtered_dataset = FilteredDataset(dummy_dataset, indices)
     result = list(filtered_dataset)
     answer = [list(dummy_dataset)[2], list(dummy_dataset)[4]]
@@ -106,7 +106,7 @@ def test_filtered_dataset_iteration(dummy_dataset):
 
 def test_filtered_dataset_empty_indices(dummy_dataset):
     def indices():
-        return lambda x: []
+        return []
     filtered_dataset = FilteredDataset(dummy_dataset, indices)
     result = list(filtered_dataset)
     assert result == []
@@ -114,7 +114,7 @@ def test_filtered_dataset_empty_indices(dummy_dataset):
 
 def test_filtered_dataset_length(dummy_dataset):
     def indices():
-        return lambda x: [1, 5]
+        return [1, 5]
     filtered_dataset = FilteredDataset(dummy_dataset, indices)
     assert len(filtered_dataset) == len(list(filtered_dataset))
     assert 2 == len(filtered_dataset)
