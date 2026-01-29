1fe6202a63f901f4252cecab436d073dbf8c2086
stijn <s.van.lierop@nfi.nl>
2026-01-29 17:04:09 +0100

    fix formatting

--- deepfake_detection/models/__init__.py
+++ deepfake_detection/models/__init__.py
@@ -1,3 +1,3 @@
-from deepfake_detection.models.model import Model
-from deepfake_detection.models.model import TrainableMixin
+from deepfake_detection.models.model import Model as Model
+from deepfake_detection.models.model import TrainableMixin as TrainableMixin
 from deepfake_detection.models.prediction import Prediction as Prediction
