7c34ac7a6dcc45da79b94e06e519bd741ee1da8f
stijn <s.van.lierop@nfi.nl>
2026-05-15 17:22:19 +0200

    fix module paths

--- deepfake_detection/models/localization/__init__.py
+++ deepfake_detection/models/localization/__init__.py
@@ -1,5 +1,5 @@
-from models.localization.trufor import TruFor
-from models.localization.segformer import SegFormerDetector
+from deepfake_detection.models.localization.trufor import TruFor
+from deepfake_detection.models.localization.segformer import SegFormerDetector
 
 
 __all__ = ["TruFor", "SegFormerDetector"]
